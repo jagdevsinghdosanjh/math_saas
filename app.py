1dd7b117976dd5b4e65bdced97df0e90ce424e4a
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-07 10:11:52 +0530

    Ok

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 ﻿﻿import streamlit as st
-
 from math_saas.auth import logout, app_container_style
 from math_saas.admin.admin_app import run_admin
 from math_saas.student.student_app import run_student
 from math_saas.utils.db import get_supabase
-
 
 # -----------------------------
 # GENERIC LOGIN HANDLER
