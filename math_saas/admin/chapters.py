1fb7fb380e27a06a9ff90ba662a5043738046fcc
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-07 13:57:50 +0530

    Ok

--- math_saas/admin/chapters.py
+++ math_saas/admin/chapters.py
@@ -1,4 +1,4 @@
-﻿﻿import streamlit as st
+import streamlit as st
 from math_saas.utils.db import get_supabase
 
 def render():
@@ -26,42 +26,3 @@ def render():
                 "chapter_name": chapter_name
             }).execute()
             st.success("Chapter added. Refresh to see changes.")
-
-# import streamlit as st
-# from math_saas.utils.db import get_supabase
-
-# def render():
-#     st.header("Chapters")
-
-#     supabase = get_supabase()
-#     res = (
-#         supabase.table("chapters")
-#         .select("*")
-#         .order("created_at", desc=True)
-#         .execute()
-#     )
-#     chapters = res.data or []
-
-#     st.subheader("All Chapters")
-#     st.dataframe(chapters, width="stretch")
-
-#     st.subheader("Add Chapter")
-#     with st.form("add_chapter"):
-#         title = st.text_input("Title")
-#         slug = st.text_input("Slug")
-#         description = st.text_area("Description")
-#         is_published = st.checkbox("Published", value=True)
-#         submitted = st.form_submit_button("Create")
-#         if submitted:
-#             if not title or not slug:
-#                 st.error("Title and slug are required.")
-#             else:
-#                 supabase.table("chapters").insert(
-#                     {
-#                         "title": title,
-#                         "slug": slug,
-#                         "description": description,
-#                         "is_published": is_published,
-#                     }
-#                 ).execute()
-#                 st.success("Chapter created. Refresh to see changes.")
