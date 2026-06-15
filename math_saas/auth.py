26f3fdcb5778e0ebfc03860a48e34dfc8f0b02e7
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-16 00:49:29 +0530

    Ok

--- math_saas/auth.py
+++ math_saas/auth.py
@@ -3,8 +3,8 @@ import re
 from typing import Any, Dict, List, cast
 from math_saas.utils.db import get_supabase
 
-TEXT_MUTED = "#a0a6b1"
-TEXT_MAIN = "#f8f9fa"
+TEXT_MUTED = "#f4751a"
+TEXT_MAIN = "#3291f1"
 ACCENT = "#00ff88"
 
 
