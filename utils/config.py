e4b0b4877c46a3862469f55a4e0f3617da56b38b
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-26 01:47:20 +0530

    Ok

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,9 @@
-USE_OLLAMA = "true"
+# config.py
+
+USE_OLLAMA = True
+
 OLLAMA_URL = "https://ollama.jsdmath.in/api/chat"
+
+# Models (must be installed on your server)
 OLLAMA_MODEL_MATH = "deepseek-r1:1.5b"
 OLLAMA_MODEL_SUMMARY = "deepseek-r1:1.5b"
