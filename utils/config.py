be6bdfc5a4d23a29c6dc61351dc8c8476b73b697
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-25 12:24:28 +0530

    Ok

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
 USE_OLLAMA = "true"
-OLLAMA_URL = "https://ollama.jsdmath.in/api/propmt"
+OLLAMA_URL = "https://ollama.jsdmath.in/api/generate"
 OLLAMA_MODEL_MATH = "deepseek-r1:1.5b"
 OLLAMA_MODEL_SUMMARY = "deepseek-r1:1.5b"
